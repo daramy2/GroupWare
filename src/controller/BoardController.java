7316284950ec2b047516274b43d17a7e7f12f0ff
seongeun2 <se5319@naver.com>
2018-03-28 11:27:12 +0900

    03.28_1111

--- src/controller/BoardController.java
+++ src/controller/BoardController.java
@@ -1,5 +1,21 @@
 package controller;
 
-public class BoardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/board")
+
+public class BoardController{
+	
+	@RequestMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("message", "/board/index");
+		return "index";
+	}
+	
+	
 
 }
